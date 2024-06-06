617e6ca4f90699e84c7627c9c18bdeb9609dae8d
Gitmahi-js <maheshbagali001@gmail.com>
2024-06-06 16:44:45 +0530

    basic python operations

---
+++ hello.py
@@ -0,0 +1,24 @@
+#import sys
+#print(sys.version)
+
+#print("Hello python!")
+
+#a=5
+#b=6
+#print(a+b)
+
+#multilinestring = """This is a multiline string.
+#It has 3 lines.
+#The last line says Bye
+#"""
+#print(multilinestring)
+
+"""
+This is a multiline comment.
+So it has more than one line
+"""
+
+x=3
+y="name"
+print(type(x))
+print(type(y))
